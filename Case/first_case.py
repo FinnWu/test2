64c0a86dc94aa1e32241a4f600af37b84253494a
ssgitou <ssgitou@gmail>
2019-02-26 22:09:29 +0800

    PO模型

---
+++ Case/first_case.py
@@ -0,0 +1,7 @@
+#这个文件下是测试用例层，就是去执行操作
+
+
+
+git config --global user.name "ssgitou"
+git config --global user.email "ssgitou@gmail"
+ssh-keygen -t rsa -C "ssgitou@gmail"
---
+++ Handle/register_handle.py
@@ -0,0 +1,17 @@
+#handle文件下都是操作层，写的是操作流程,我的理解就是你要输入用户名这里是要怎么去操作
+class LoginHandle(object):
+    #输入邮箱
+    def send_user_email(self):
+        pass
+    #输入用户名
+    def send_user_name(self):
+        pass
+    #输入密码
+    def send_user_password(self):
+        pass
+    #输入验证码
+    def send_code(self):
+        pass
+    #点击注册按钮
+    def send_login_button(self):
+        pass
